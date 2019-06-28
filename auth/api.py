688837a0e93c6aa6f404354a143f64c4127773cb
code-to-gold <jayden.park@paust.io>
2019-06-28 19:45:30 +0900

    Add post API for generating token

--- auth/api.py
+++ auth/api.py
@@ -1,12 +1,34 @@
 from flask.views import MethodView
-from flask import request, abort
+from flask import request, abort, jsonify
+from jsonschema import Draft7Validator
+from jsonschema.exceptions import best_match
+import jwt
+import uuid
+
+from auth.schema import schema
+from config import AuthConfig
+from amo_storage import redis
 
 
 class AuthAPI(MethodView):
+
     def __init__(self):
         if request.method != 'POST':
             abort(405)
 
     def post(self):
-        pass
+        auth_json = request.json
 
+        error = best_match(Draft7Validator(schema).iter_errors(auth_json))
+        if error:
+            return jsonify({'error': error.message}), 400
+
+        auth_json['iss'] = AuthConfig.ISSUER
+        auth_json['jti'] = str(uuid.uuid4())
+
+        encoded_jwt = jwt.encode(payload=auth_json,
+                                 key=AuthConfig.SECRET,
+                                 algorithm=AuthConfig.ALGORITHM).decode('utf-8')
+        redis.set(auth_json.get('user') + ':' + auth_json.get('operation'), encoded_jwt)
+
+        return jsonify({'token': encoded_jwt}), 200
