1b106ba0c3a665887eee832bd59f532f4eb6127b
code-to-gold <jayden.park@paust.io>
2019-07-03 14:04:38 +0900

    Change name of the column since the name metadata is reserved

--- models/metadata.py
+++ models/metadata.py
@@ -4,4 +4,4 @@ from sqlalchemy.types import JSON
 
 class MetaData(db.Model):
     parcel_id = db.Column(db.String(256), primary_key=True)
-    metadata = db.Column(JSON)
+    parcel_meta = db.Column(JSON)
