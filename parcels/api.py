12a1398c929e64a70f211daed40fd2497688530e
code-to-gold <jayden.park@paust.io>
2019-08-02 15:03:20 +0900

    Fix bug and make amo-abci's response code as 0 if key 'code' does not exist

--- parcels/api.py
+++ parcels/api.py
@@ -17,6 +17,7 @@ from flask import current_app
 from models.metadata import MetaData
 from models.ownership import Ownership
 
+
 class ParcelsAPI(MethodView):
 
     decorators = [auth_required]
@@ -39,11 +40,14 @@ class ParcelsAPI(MethodView):
             "method": "abci_query",
             "params": {
                 "path": "/usage",
-                "data": json.dumps({"buyer": buyer, "target": parcel_id}).hex()
+                "data": str.encode(json.dumps({"buyer": buyer, "target": parcel_id})).hex()
             }
         })
 
-        endpoint = self._end_point(current_app.config.AmoBlockchainNodeConfig["HOST"], str(current_app.config.AmoBlockchainNodeConfig["PORT"]))
+        endpoint = self._end_point(
+            current_app.config.AmoBlockchainNodeConfig["HOST"],
+            str(current_app.config.AmoBlockchainNodeConfig["PORT"])
+        )
 
         res = requests.post(endpoint, data=request_body, headers=request_headers)
         return res
@@ -86,15 +90,15 @@ class ParcelsAPI(MethodView):
         # Download operation
         else:
             # AMO blockchain based ACL. Quotation will be removed after amo-client is developed.
-            """
             res = self._usage_query(parcel_id, g.user)
             res_json = res.json()
             if res_json.get("error"):
                 return jsonify({"error": res_json.get("error")}), 502
 
-            if int(res_json.get("result").get("code")) != 0:
+            print(res_json)
+            if int(res_json.get('result').get('response').get('code', 0)) != 0:
                 return jsonify({"error": "No permission to download data parcel %s" % parcel_id}), 403
-            """
+
             try:
                 data = ceph.download(parcel_id).hex()
                 self._delete_key(request)
