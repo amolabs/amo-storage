84b54f183182361e2227e67fbc2e94bb94608603
code-to-gold <jayden.park@paust.io>
2019-08-05 12:08:52 +0900

    Create database automatically if database does not exist

--- amo_storage.py
+++ amo_storage.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_redis import Redis
 from adapter.adapter import CephAdapter
 from flask_iniconfig import INIConfig
+from sqlalchemy_utils.functions import database_exists
 
 db = SQLAlchemy()
 redis = Redis()
@@ -22,16 +23,23 @@ def create_app(**config_overrides):
     app.config.from_inifile(config_path, objectify=True)
 
     app.config.update(app.config.AmoStorageConfig)
+    app.config.update(app.config.CephConfig)
     app.config.update(config_overrides)
 
     db.init_app(app)
     redis.init_app(app)
 
+    if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
+        with app.app_context():
+            from models.metadata import MetaData
+            from models.ownership import Ownership
+            db.create_all()
+
     ceph.connect(
-        host=app.config.CephConfig["HOST"],
-        port=app.config.CephConfig["PORT"],
-        keyfile_path=app.config.CephConfig["KEY_FILE_PATH"],
-        default_bucket_name=app.config.CephConfig["BUCKET_NAME"]
+        host=app.config["HOST"],
+        port=app.config["PORT"],
+        keyfile_path=app.config["KEY_FILE_PATH"],
+        default_bucket_name=app.config["BUCKET_NAME"]
     )
 
     from auth.views import auth_app
