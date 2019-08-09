d3a1c02bf8df3e655b2b7533cd69d85811be8db2
AHN Jaehong <monsieur.ahn@gmail.com>
2019-08-09 15:13:45 +0900

    Script for testing general functions of amo-storage

---
+++ test_script/all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script is for testing general functions
+# of amo-storage including 'upload', 'download'
+# with generating transactions on amo-blockchain
+
+ROOT=$(dirname $0)
+
+# parcel test
+$ROOT/parcel.sh
