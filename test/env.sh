21250cea83d03b260579d63fc9bbac699b4d19e4
AHN Jaehong <monsieur.ahn@gmail.com>
2019-08-12 10:16:13 +0900

    Pre-fix testnet-docker CephConfig's HOST
    - remove 'exclusion of t0' condition on env.sh

--- test/env.sh
+++ test/env.sh
@@ -9,5 +9,5 @@ OPT="$RPC $STO"
 SLEEP=0.5
 testfile=test_script/testfile
 
-eval $(amocli key list | awk '{ if ($2 != "t0") printf "%s=%s\n",$2,$4 }')
+eval $(amocli key list | awk '{ printf "%s=%s\n",$2,$4 }')
 faucet=BEFF22606A9FB730455736E7B33C846171F2865C
